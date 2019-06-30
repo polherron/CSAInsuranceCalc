3c5591959aa447f51dda93381d4e300f3c4d3d64
polherron <pol.herron@gmail.com>
2019-06-30 14:32:05 +0100

    Update to selection box text select code rempval of platinum customer code

--- CSAInsuranceCalc/frmQuote.cs
+++ CSAInsuranceCalc/frmQuote.cs
@@ -54,7 +54,7 @@ namespace CSAInsuranceCalc
            
 
             int.TryParse(txtCustomerID.Text, out customerNumber);
-            bool blacklisted = Utilities.CheckBlacklisted(cbStates.SelectedText.ToString(), customerNumber);
+            bool blacklisted = Utilities.CheckBlacklisted(cbStates.Text, customerNumber);
             bool over25 = Utilities.CheckOver25(dtPicker.Value);
             
             if (!blacklisted && over25)
